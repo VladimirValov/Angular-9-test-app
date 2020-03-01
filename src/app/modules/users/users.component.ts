968252e56ce47eb0d84ec79c6e8724bf234c22f1
VladimirValov <vvalov23@gmail.com>
2020-03-01 22:00:23 +0200

    add users markup

--- src/app/modules/users/users.component.ts
+++ src/app/modules/users/users.component.ts
@@ -1,15 +1,111 @@
 import { Component, OnInit } from '@angular/core';
 
+const mockUsers = [
+  {
+    avatar:
+      'https://sun1-20.userapi.com/c850220/v850220499/721a2/QJPqmsQQSmU.jpg',
+    name: 'Иван',
+    fname: 'Иванов',
+    mname: 'Иванович',
+    balance: 100,
+    date: new Date(),
+    status: 1
+  },  {
+    avatar:
+      'https://sun1-20.userapi.com/c850220/v850220499/721a2/QJPqmsQQSmU.jpg',
+    name: 'Иван',
+    fname: 'Иванов',
+    mname: 'Иванович',
+    balance: 100,
+    date: new Date(),
+    status: 1
+  },  {
+    avatar:
+      'https://sun1-20.userapi.com/c850220/v850220499/721a2/QJPqmsQQSmU.jpg',
+    name: 'Иван',
+    fname: 'Иванов',
+    mname: 'Иванович',
+    balance: 100,
+    date: new Date(),
+    status: 1
+  },  {
+    avatar:
+      'https://sun1-20.userapi.com/c850220/v850220499/721a2/QJPqmsQQSmU.jpg',
+    name: 'Иван',
+    fname: 'Иванов',
+    mname: 'Иванович',
+    balance: 100,
+    date: new Date(),
+    status: 1
+  },  {
+    avatar:
+      'https://sun1-20.userapi.com/c850220/v850220499/721a2/QJPqmsQQSmU.jpg',
+    name: 'Иван',
+    fname: 'Иванов',
+    mname: 'Иванович',
+    balance: 100,
+    date: new Date(),
+    status: 1
+  },  {
+    avatar:
+      'https://sun1-20.userapi.com/c850220/v850220499/721a2/QJPqmsQQSmU.jpg',
+    name: 'Иван',
+    fname: 'Иванов',
+    mname: 'Иванович',
+    balance: 100,
+    date: new Date(),
+    status: 1
+  },  {
+    avatar:
+      'https://sun1-20.userapi.com/c850220/v850220499/721a2/QJPqmsQQSmU.jpg',
+    name: 'Иван',
+    fname: 'Иванов',
+    mname: 'Иванович',
+    balance: 100,
+    date: new Date(),
+    status: 1
+  },  {
+    avatar:
+      'https://sun1-20.userapi.com/c850220/v850220499/721a2/QJPqmsQQSmU.jpg',
+    name: 'Иван',
+    fname: 'Иванов',
+    mname: 'Иванович',
+    balance: 100,
+    date: new Date(),
+    status: 1
+  },  {
+    avatar:
+      'https://sun1-20.userapi.com/c850220/v850220499/721a2/QJPqmsQQSmU.jpg',
+    name: 'Иван',
+    fname: 'Иванов',
+    mname: 'Иванович',
+    balance: 100,
+    date: new Date(),
+    status: 1
+  },  {
+    avatar:
+      'https://sun1-20.userapi.com/c850220/v850220499/721a2/QJPqmsQQSmU.jpg',
+    name: 'Иван',
+    fname: 'Иванов',
+    mname: 'Иванович',
+    balance: 100,
+    date: new Date(),
+    status: 1
+  },
+];
+const auth = {
+  login: 'test@example.com',
+  password: '1q2w3e'
+};
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  users = mockUsers;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
